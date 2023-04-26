abf8e3cc57268ad88305086e60822984eaaef410
Teek3r <lbthien5@gmail.com>
2023-04-26 12:50:17 +0700

    Modified

--- js/myScript.js
+++ js/myScript.js
@@ -1,14 +1,11 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
-
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -16,7 +13,7 @@ function showSlides(n) {
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "" ;
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
