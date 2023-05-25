96bc2933cb9bf509bf4ea78289f0d628876ee9aa
hohoanghuy13 <hohoanghuy170@gmail.com>
2023-05-25 08:45:48 +0700

    Jquery-content4 + up web

---
+++ js/showNewsJquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#li-1").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/rC3tioGPcXg")
+    })
+    $("#li-2").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/e3ai7rH-TAQ")
+    })
+    $("#li-3").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/jAp4AG0mC0g")
+    })
+    $("#li-4").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/lvUq-uiTNHk")
+    })
+    $("#li-5").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/hUlpex9jBvc")
+    })
+    $("#li-6").click(function(){
+        $("#video-col1 iframe").attr("src", "https://www.youtube.com/embed/ugA9xE_qaKs")
+    })
+})
